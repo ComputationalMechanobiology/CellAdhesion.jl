4f4dee78f9a1b6a59c5f2a49492933d5c1c635d6
ep670 <ep670@cam.ac.uk>
2026-03-15 19:17:38 +0000

    fixed comment types in loops

--- docs/src/example_junction_with_stress_from_file.jl
+++ docs/src/example_junction_with_stress_from_file.jl
@@ -3,7 +3,7 @@ using DelimitedFiles
 using Plots
 using Statistics
 
-##find all the files in the data directory
+# find all the files in the data directory
 files = readdir(joinpath(@__DIR__, "example_data"))
 
 # extract the strain rates from the file names
@@ -14,7 +14,6 @@ sorted_idx = sortperm(strain_rates)
 files = files[sorted_idx]
 strain_rates = strain_rates[sorted_idx]
 
-## Define parameters of the junction 
 # Define the bond model (slip bond model)
 model = SlipBondModel((k_on_0=3e-3,), (k_off_0=3e-4, f_1e=0.055))
 
@@ -26,7 +25,7 @@ l = 1.0               # Distance between bonds
 n = 50                      # Number of simulations for each strain rate
 n_f = length(strain_rates)  # Number of different strain rates to apply to the junction
 
-## run simulation for each strain rate and save the results in a dictionary
+# run simulation for each strain rate and save the results in a dictionary
 time_break = zeros(n_f)
 stress_break = zeros(n_f)
 strain_break = zeros(n_f)
@@ -35,7 +34,7 @@ p1 = plot()
 p2 = plot()
 p3 = plot()
 for (i, strain_rate) in enumerate(strain_rates)
-    # load the stress-time data from the file
+    ## load the stress-time data from the file
     data = readdlm(joinpath(@__DIR__, "example_data", files[i]))
     time = data[2:end, 1]   # ignore the header
     strain = data[2:end, 2]
@@ -43,7 +42,7 @@ for (i, strain_rate) in enumerate(strain_rates)
     dt = time[2] - time[1]  # Calculate time step from the data
     max_steps = length(time)  # Set max_steps based on the length of the time data
     
-    # convert stress to tension
+    ## convert stress to tension
     tension = stress .* N .* l  # Assuming stress is per bond, multiply by number of bonds and distance to get tension
 
     tension_break_v = zeros(n)
@@ -51,24 +50,24 @@ for (i, strain_rate) in enumerate(strain_rates)
     strain_break_v = zeros(n)
 
     for repeat in 1:n
-        # Define the junction
+        ## Define the junction
         x = Cluster(N, l, model, :force_global)
 
-        # Run the simulation
+        ## Run the simulation
         state, tension_break_v[repeat], time_break_v[repeat], step = runcluster(x, tension, dt; max_steps=max_steps, verbose=true)
     end
     tension_break_v /= N*l # Convert tension back to stress for plotting
     strain_break_v = [strain[argmin(abs.(time .- time_break_v[i]))] for i in 1:n]
 
-    # Scatter plot of rupture times for the current strain rate
+    ## Scatter plot of rupture times for the current strain rate
     scatter!(p1, strain_break_v, tension_break_v , label="", mc="#1E88E5", ms=4, ma=1)
     scatter!(p2, repeat([strain_rate], n), time_break_v, label="", mc="#1E88E5", ms=4, ma=1)
     scatter!(p3, repeat([strain_rate], n), tension_break_v , label="", mc="#1E88E5", ms=4, ma=1)
     
-    # add the stress-strain curve for the current strain rate
+    ## add the stress-strain curve for the current strain rate
     plot!(p1, strain, stress, label="", lw=2, color=:grey)
 
-    # compute the means
+    ## compute the means
     time_break[i] = mean(time_break_v)
     stress_break[i] = mean(tension_break_v)
     strain_break[i] = mean(strain_break_v)
@@ -84,4 +83,3 @@ plot!(p3, xlabel="Strain Rate (1/s)", ylabel="Rupture Stress (\$N/m^2\$)", yscal
 plot!(p1, strain_break, stress_break, label="", c="#D81B60", linewidth=3, linestyle=:dash)
 
 plot(p1, p2, p3, layout=(1,3), size=(1200,400), margin=5Plots.mm)
-savefig("strain_files_ruptre_times.svg")
